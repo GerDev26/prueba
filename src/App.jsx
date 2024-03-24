1424f215807d88be0f8a8ca25c9370cbf25e2b46
German <germancanteros26@gmail.com>
2024-03-24 18:08:25 -0300

    Se esta probando el useState en los custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,101 @@
 import { useEffect } from 'react'
-import './App.css'
 import { useState } from 'react'
+import './App.css'
 
-const PRODUCTS_API = "https://api.escuelajs.co/api/v1/products"
+function getRamdomLetter() {
+  const letras = 'abcdefghijklmnopqrstuvwxyz';
+  const indiceAleatorio = Math.floor(Math.random() * letras.length);
+  return letras.charAt(indiceAleatorio);
+}
 
-export default function App() {
-  const [response, setResponse] = useState(null)
+function useProducts() {
+  const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    fetch(PRODUCTS_API)
+    fetch("https://api.escuelajs.co/api/v1/products")
       .then(res => res.json())
-      .then(data => {
-        const productImages = data.map((product, index) => (
-          product.images
-        ))
-        setResponse(productImages)
+      .then(json => {
+        setProducts(json);
+        console.log("peticion correcta")
       })
-    
-  }, [])
+      .catch(error => {
+        console.log("Error en la petición: ", error);
+      });
+  }, []);
+
+  const productsWidthImage = products
+    ?  products.filter(product => (
+        product.images[ 0] !== ('[\"https://placeimg.com/640/480/any\"]' || undefined)
+      ))
+    : []
+
+  const productsFormatted = productsWidthImage ? productsWidthImage.map(product => ({
+    id: product.id,
+    title: product.title,
+    price: product.price,
+    image: product.images[0]
+  })) : [];
+
+  return productsFormatted
+  
+}
+
+function useSearchCocktails(initialLetter){
+  
+  const [letter, setLetter] = useState(initialLetter);
+
+  const COCKTAIL_API = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f="
+
+  const [cocktails, setCocktails] = useState(null)
+
+  useEffect(() => {
+    fetch(COCKTAIL_API + letter)
+      .then(res => res.json())
+      .then(json => {
+        setCocktails(json.drinks);
+        
+        console.log("peticion correcta")
+      })
+      .catch(error => {
+        console.log("Error en la petición: ", error);
+      });
+  }, [letter]);
+
+  const changeLetter = (newLetter) => {
+    setLetter(newLetter)
+  }
+
+  const listOfCocktails = cocktails
+    ? cocktails.map(cocktail => ({
+      id: cocktail.idDrink,
+      name: cocktail.strDrink,
+      image: cocktail.strDrinkThumb
+    }))
+    : []
+
+    return [listOfCocktails, changeLetter]
+}
+export default function App() {
+
+  const [cocktails, setCocktails] = useSearchCocktails()
+
+  const handleClick = () => {
+    setCocktails(getRamdomLetter())
+  }
 
   return(
       <div className='gallery'>
-        {response ? 
-        response.map((imagen, index) => (
-          <img key={index} src={imagen[0]} alt={`Imagen ${index}`} />
+        <button onClick={handleClick}>Buscar</button>
+        {cocktails ? 
+        cocktails.map((product) => (
+          <div key={product.id} >
+            <p>{product.name}</p>
+            <img src={product.image} alt={`Imagen ${product.id}`} />
+          </div>
         ))
         : <h1>Cargando...</h1>
       }
-      </div>
+        </div>
     )
 }
 
---
+++ src/services/myAPIs.js
@@ -0,0 +1,4 @@
+
+export const COCKTAILS_API = "www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+
+export const PRODUCTS_API = "https://api.escuelajs.co/api/v1/products"
