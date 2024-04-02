799ba84956a409a2c89c3b5670e507ff90e429e0
German <germancanteros26@gmail.com>
2024-04-02 20:45:14 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
-import './App.css'
-import { useSearchCocktails } from './hooks/cocktails/useCocktails'
-import { CocktailCard } from './components/CocktailCard/CocktailCard'
-import { LoadingCocktails } from './components/CocktailCard/LoadingCocktails'
-import { useEffect, useRef } from 'react'
-
+import { Route, Routes} from 'react-router-dom'
+import { Home } from './pages/home'
 
 export default function App() {
 
-  const [cocktails, setCocktails] = useSearchCocktails("")
-
-  const search = useRef()
-
-  const handleClick = (event) => {
-    event.preventDefault()
-    const fields = Object.fromEntries(new window.FormData(event.target));
-
-    const minusFields = Object.values(fields).map(field => (
-      field.toLowerCase()
-    ))
-    console.log(minusFields)
-  };
-
-  const handleChange = () => {
-    const value = search.current.value
-    console.log(value)
-    setCocktails(value)
-  }
-
   return(
     <>
-      <form onSubmit={handleClick}>
-        <input ref={search} onChange={handleChange} name="1" type="text"/>
-        <input name="2" type="text"/>
-        <input name="3" type="text"/>
-        <button>Buscar</button>
-      </form>
-
-
-      <div className="gallery">
-          {cocktails 
-            ? cocktails.map((cocktail) => ( <CocktailCard key={cocktail.id} id={cocktail.id} name={cocktail.name} image={cocktail.image}/>))
-            : <LoadingCocktails/>
-          }
-      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
-    )
+  )
+
+
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,48 @@
+import './home.css'
+import { useSearchCocktails } from '../hooks/cocktails/useCocktails.js'
+import { CocktailCard } from '../components/CocktailCard/CocktailCard'
+import { LoadingCocktails } from '../components/CocktailCard/LoadingCocktails'
+import { useRef } from 'react'
+
+export function Home(){
+    const [cocktails, setCocktails] = useSearchCocktails("");
+
+    const search = useRef();
+    
+    const handleClick = (event) => {
+      event.preventDefault();
+      const fields = Object.fromEntries(new window.FormData(event.target));
+    
+      const minusFields = Object.values(fields).map(field => (
+        field.toLowerCase()
+      ));
+      console.log(minusFields);
+    };
+    
+    const handleChange = () => {
+      const value = search.current.value;
+      console.log(value);
+      setCocktails(value);
+    };
+    
+    return (
+      <>
+        <form onSubmit={handleClick}>
+          <input ref={search} onChange={handleChange} name="1" type="text"/>
+          <input name="2" type="text"/>
+          <input name="3" type="text"/>
+          <button>Buscar</button>
+        </form>
+    
+        <div className="gallery">
+          {cocktails 
+            ? cocktails.map((cocktail) => (
+                <CocktailCard key={cocktail.id} id={cocktail.id} name={cocktail.name} image={cocktail.image}/>
+              ))
+            : <LoadingCocktails/>
+          }
+        </div>
+      </>
+    );
+    
+}
